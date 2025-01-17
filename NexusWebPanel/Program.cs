f97fd1d1ee0ffcf353d866bad2be5820751dff3a
hisazakura <luthfanarrayyan@gmail.com>
2025-01-17 17:26:29 +0700

    add basic web panel (not yet integrated)

--- NexusWebPanel/Program.cs
+++ NexusWebPanel/Program.cs
@@ -1,14 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.SignalR;
+using NexusWebPanel.Hubs;
+using NexusWebPanel.Services;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -20,6 +25,18 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<LogHub>("/logs");
+
+WebsocketListener listener = new(8080);
+listener.Start();
+
+IHubContext<LogHub> hubContext = app.Services.GetRequiredService<IHubContext<LogHub>>();
+
+listener.MessageReceived += message =>
+    hubContext.Clients.All.SendAsync("ReceiveMessage", message);
 
 app.Run();
